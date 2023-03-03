a4e4df0bb2290df00e76b7e40926e3cf1ce5efb3
TomassAli <testtomass@gmail.com>
2023-03-03 16:20:31 +0200

    update

--- scripts/quiz-game.js
+++ scripts/quiz-game.js
@@ -41,8 +41,8 @@ function loadQuestion(topic, points, question, choices, answer) {
   }
 
 
-  function generateLayout() {
-    const gameLayout = `
+  function generateTitles() {
+    const gameTitles = `
     <div class="overlay">
     <div class="game-wrapper">
         <h1 class="game-name">Zināšanu Cīņas<br>
@@ -72,468 +72,52 @@ function loadQuestion(topic, points, question, choices, answer) {
       <div class="grid--title">Mūziķi</div>
       <div class="grid--title">Dzīvnieki</div>
       <div class="grid--title">Cilvēki</div>
-
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontgeo200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Ģeogrāfija</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" onclick="loadQuestion('Ģeogrāfija', '200', 'Kura ir Francijas Galvas Pilsēta?', ['Parīze', 'Berlīne', 'Madride'], 'Parīze')">JAUTĀJUMS</p>
-            <param id="first-question" value="Ludza">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p200" value="200">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontd200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Dziesmas</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" id="d200">JAUTĀJUMS</p>
-            <param id="second-question" value="Baumaņu Kārlis">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p200" value="200">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="fronta200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Aktieri</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" id="a200">JAUTĀJUMS</p>
-            <param id="third-question" value="Leonardo Di Kaprio">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p200" value="200">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="musician200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Mūziķi</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" id="m200">JAUTĀJUMS</p>
-            <param id="forth-question" value="Ģitāru">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p200" value="200">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="animal200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Dzīvnieki</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" id="dz200">JAUTĀJUMS</p>
-            <param id="fifth-question" value="Žirafe">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p200" value="200">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="peopl200">200
-          </div>
-          <div class="flip-card-back">
-            <h1>Cilvēki</h1> 
-            <p class="points">200 Punkti</p> 
-            <p class="question" id="c200">JAUTĀJUMS</p>
-            <param id="sixth-question" value="Deguns">
-          </div>
-        </div>
-      </div>
-
-      <!-- 200 END -->
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontgeo400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Ģeogrāfija</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="g400">JAUTĀJUMS</p>
-            <param id="seventh-question" value="Āzija">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontd400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Dziesmas</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="d400">JAUTĀJUMS</p>
-            <param id="eith-question" value="Pop">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="fronta400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Aktieri</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="a400">JAUTĀJUMS</p>
-            <param id="ninth-question" value="Anthony Hopkins">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="musician400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Mūziķi</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="m400">JAUTĀJUMS</p>
-            <param id="tenth-question" value="Madonna">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="animal400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Dzīvnieki</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="dz400">JAUTĀJUMS</p>
-            <param id="eleventh-question" value="Āfrikas Zīlonis">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p400" value="400">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="peopl400">400
-          </div>
-          <div class="flip-card-back">
-            <h1>Cilvēki</h1> 
-            <p class="points">400 Punkti</p> 
-            <p class="question" id="c400">JAUTĀJUMS</p>
-            <param id="twelveth-question" value="Āda">
-          </div>
-        </div>
-      </div>
-
-      <!-- 400 END -->
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontgeo600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Ģeogrāfija</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="g600">JAUTĀJUMS</p>
-            <param id="trnth-question" value="Parīze">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontd600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Dziesmas</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="d600">JAUTĀJUMS</p>
-            <param id="frtnth-question" value="Michael Jackson - Billie Jean">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="fronta600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Aktieri</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="a600">JAUTĀJUMS</p>
-            <param id="fifthnth-question" value="Marlon Brando">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="musician600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Mūziķi</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="m600">JAUTĀJUMS</p>
-            <param id="sixtnth-question" value="Freddie Mercury">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="animal600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Dzīvnieki</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="dz600">JAUTĀJUMS</p>
-            <param id="seventhn-question" value="Gepards">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p600" value="600">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="peopl600">600
-          </div>
-          <div class="flip-card-back">
-            <h1>Cilvēki</h1> 
-            <p class="points">600 Punkti</p> 
-            <p class="question" id="c600">JAUTĀJUMS</p>
-            <param id="eitn-question" value="Ciskas kauls">
-          </div>
-        </div>
-      </div>
-
-      <!-- 600 END -->
-
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontgeo800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Ģeogrāfija</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="g800">JAUTĀJUMS</p>
-            <param id="nith-question" value="Nīla">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontd800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Dziesmas</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="d800">JAUTĀJUMS</p>
-            <param id="twenth-question" value="Billie Eilish - Bad Guy">
-          </div>
-        </div>
-      </div>
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="fronta800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Aktieri</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="a800">JAUTĀJUMS</p>
-            <param id="twenthone-question" value="Harrison Ford">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="musician800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Mūziķi</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="m800">JAUTĀJUMS</p>
-            <param id="twenthtwo-question" value="Mick Jagger">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="animal800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Dzīvnieki</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="dz800">JAUTĀJUMS</p>
-            <param id="twenththree-question" value="Zobenzivis">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p800" value="800">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="peopl800">800
-          </div>
-          <div class="flip-card-back">
-            <h1>Cilvēki</h1> 
-            <p class="points">800 Punkti</p> 
-            <p class="question" id="c800">JAUTĀJUMS</p>
-            <param id="twenthtfourth-question" value="Āda">
-          </div>
-        </div>
-      </div>
-
-      <!-- 800 END -->
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontgeo1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Ģeogrāfija</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="g1000">JAUTĀJUMS</p>
-            <param id="twenthtfifth-question" value="Ngerulmuda">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="frontd1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Dziesmas</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="d1000">JAUTĀJUMS</p>
-            <param id="twentysix-question" value="Wiz Khalifa">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="fronta1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Aktieri</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="a1000">JAUTĀJUMS</p>
-            <param id="twentyseven-question" value="Mirdza Martinsone">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="musician1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Mūziķi</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="m1000">JAUTĀJUMS</p>
-            <param id="twentyei-question" value="Cant Help Falling In Love">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="animal1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Dzīvnieki</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="dz1000">JAUTĀJUMS</p>
-            <param id="twentynine-question" value="Kolonija">
-          </div>
-        </div>
-      </div>
-
-
-      <div class="flip-card">
-        <param id="p1000" value="1000">
-        <div class="flip-card-inner">
-          <div class="flip-card-front" id="peopl1000">1000
-          </div>
-          <div class="flip-card-back">
-            <h1>Cilvēki</h1> 
-            <p class="points">1000 Punkti</p> 
-            <p class="question" id="c1000">JAUTĀJUMS</p>
-            <param id="thirtnh-question" value="20%">
-          </div>
-        </div>
-      </div>
-
     </div>
   `
-  const loadLayout = document.createElement("div")
-  loadLayout.innerHTML = gameLayout
-  document.getElementById("quiz-game").appendChild(loadLayout)
+  const loadTitles = document.createElement("div")
+  loadTitles.innerHTML = gameTitles
+  document.getElementById("quiz-game").appendChild(loadTitles)
   }
 
-  generateLayout()
+generateTitles()
 
+function generateCards(topic) {
+  class QuestionsCards {
+    constructor(topic) {
+      this.topic = topic
+    }
+  }
+  const gameCards = `
+    <div class="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">200${this.topic}
+        </div>
+        <div class="flip-card-back">
+          <h1>Ģeogrāfija</h1> 
+          <p class="points">200 Punkti</p> 
+          <p class="question" onclick="loadQuestion('Ģeogrāfija', '200', 'Kura ir Francijas Galvas Pilsēta?', ['Parīze', 'Berlīne', 'Madride'], 'Parīze')">JAUTĀJUMS</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
+const loadCards = document.createElement("div")
+loadCards.innerHTML = gameCards
+document.getElementById("quiz-game").appendChild(loadCards)
 
+const newQuestionsCards = new QuestionsCards(topic)
+newQuestionsCards.generateCards("Ģeogrāfija")
+}
 
+// generateCards("Ģeogrāfija")
+// generateCards("Ģeogrāfija")
+// generateCards("Ģeogrāfija")
+// generateCards("Ģeogrāfija")
+// generateCards("Ģeogrāfija")
+// generateCards()
+// generateCards()
+// generateCards()
+// generateCards()
 
 
 
